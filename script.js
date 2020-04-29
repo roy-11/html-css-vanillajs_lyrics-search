0e80cd8e652302412b0108e980e133cedaff9795
ryosukehagiwara <hukurahagi1109@gmail.com>
2020-04-29 18:01:33 +0900

    add pagination logic

--- script.js
+++ script.js
@@ -15,9 +15,45 @@ form.addEventListener("submit", (e) => {
 });
 
 async function searchSongs(term) {
-  const res = await fetch(`${apiURL}/suggest/${term}`);
+  try {
+    const res = await fetch(`${apiURL}/suggest/${term}`);
+    const data = await res.json();
+    showData(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function showData({ data, prev, next }) {
+  result.innerHTML = `
+  <ul class="songs">
+    ${data
+      .map(
+        (song) => `
+    <li>
+      <span>
+        <strong>${song.artist.name}</strong> - ${song.title}
+      </span>
+      <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">歌詞を取得</button>
+    </li>
+    `
+      )
+      .join("")}
+  </ul>
+  `;
+
+  if (prev || next) {
+    more.innerHTML = `
+    ${prev ? `<button class="btn" onclick="getMoreSongs('${prev}')">戻る</button>` : ""}
+    ${next ? `<button class="btn" onclick="getMoreSongs('${next}')">進む</button>` : ""}
+    `;
+  } else {
+    more.innerHTML = "";
+  }
+}
+
+async function getMoreSongs(url) {
+  const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
   const data = await res.json();
   showData(data);
 }
-
-function showData(data) {}
